a9eca81546b250181fc41c29310040aa07316b8b
Aurelie Mahe <mahe.aureliems@gmail.com>
2019-08-16 12:36:04 +0200

    test

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,8 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
+  validates :photo, presence: true
+  mount_uploader :photo, PhotoUploader
 
   validates :name, presence: true, uniqueness: true, allow_blank: false
 end
